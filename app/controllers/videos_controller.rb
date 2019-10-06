4bf39890f68aa51f57132786bb8ca3d5033980c9
TheWillGabriel <will.gabriel@gmail.com>
2019-10-06 11:41:28 -0600

    Test video iframe

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,7 @@
 class VideosController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,19 +1,18 @@
 require 'test_helper'
 
 class VideosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get videos_index_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get videos_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get videos_create_url
     assert_response :success
   end
-
 end

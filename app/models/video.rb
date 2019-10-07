7ea9cd645765624d523cb0e7a691527587dafd6d
TheWillGabriel <will.gabriel@gmail.com>
2019-10-07 14:21:34 -0600

    Create add_details to fill in YT data before save

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,14 @@
 class Video < ApplicationRecord
+  validates :link, presence: true
+  before_save :get_details
+
+  private
+
+    def add_details
+      video = Yt::Video.new url: link
+      self.uid = video.id
+      self.title = video.title
+    rescue Yt::Errors::NoItems
+      resource.title = ''
+    end
 end

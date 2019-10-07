c098b4015f6fc6ddac64de68c8f43141456089f6
TheWillGabriel <will.gabriel@gmail.com>
2019-10-07 15:20:47 -0600

    Trim 'karaoke' and 'version' from titles

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,8 +7,22 @@ class Video < ApplicationRecord
     def add_details
       video = Yt::Video.new url: link
       self.uid = video.id
-      self.title = video.title
+      self.title = video.title.trim
     rescue Yt::Errors::NoItems
       resource.title = ''
+    end
+
+    def trim(string)
+      string.strip_karaoke_version
+            .strip_karaoke
+            .strip
+    end
+
+    def strip_karaoke_version(string)
+      string.gsub(/(K|k)araoke (V|v)ersion/, '')
+    end
+
+    def strip_karaoke(string)
+      string.gsub(/(K|k)araoke/, '')
     end
 end

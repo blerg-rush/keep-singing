01979d8b36bdd2d942425e7ad13541412362072f
TheWillGabriel <will.gabriel@gmail.com>
2019-10-08 14:30:42 -0600

    Use channel relation to find existing videos

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -10,11 +10,11 @@ class Channel < ApplicationRecord
   def update_videos
     channel = Yt::Channel.new id: uid
     channel.videos.each do |yt_video|
-      next unless Video.find_by(uid: yt_video.id).nil?
-
-      video = videos.build(uid: yt_video.id)
+      video = videos.find { |v| v.uid == yt_video.id }
+      video ||= videos.build(uid: yt_video.id)
       video.fill_details(yt_video)
-      video.save(validate: false)
     end
+
+    save
   end
 end

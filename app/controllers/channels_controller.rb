0e154fd575d6d9cec0e6f756af395751b7b98a08
TheWillGabriel <will.gabriel@gmail.com>
2019-10-08 11:31:14 -0600

    Define channel strong params

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,7 +1,15 @@
 class ChannelsController < ApplicationController
-  def index; end
+  def index
+    @channels = Channel.all
+  end
 
   def create; end
 
   def destroy; end
+
+  private
+
+    def channel_params
+      params.require(:channel).permit(:uid)
+    end
 end

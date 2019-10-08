2a60d4124fd78c3ad9cc6f5ff8b018d3f43a2f2d
TheWillGabriel <will.gabriel@gmail.com>
2019-10-08 10:36:08 -0600

    Add foreign key for initialized channel video before save

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,18 +1,17 @@
 class Channel < ApplicationRecord
-  has_many :videos
+  has_many :videos, dependent: :nullify
   validates :uid, presence: true
 
-  private
-
-    def add_videos
-      channel = YT::Channel.new id: uid
-      channel.videos.each do |yt_video|
-        video = videos.find_or_initialize_by(uid: yt_video.id)
-        video.title = yt_video.title
-        video.link = "https://www.youtube.com/watch?v=#{yt_video.id}"
-        video.description = yt_video.description
-        video.channel_uid = uid
-        video.save
-      end
+  def update_videos
+    channel = Yt::Channel.new id: uid
+    channel.videos.each do |yt_video|
+      video = videos.find_or_initialize_by(uid: yt_video.id)
+      video.title = yt_video.title
+      video.link = "https://www.youtube.com/watch?v=#{yt_video.id}"
+      video.description = yt_video.description
+      video.channel_uid = uid
+      video.channel_id = id
+      video.save(validate: false)
     end
+  end
 end

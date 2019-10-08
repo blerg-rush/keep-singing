7215946e1bc5e1cbefe6d385787e4bfb181f3027
TheWillGabriel <will.gabriel@gmail.com>
2019-10-08 12:10:56 -0600

    Delegate filling video details to Video model

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,16 +1,12 @@
 class Channel < ApplicationRecord
+  validates :uid, presence: true, uniqueness: true
   has_many :videos, dependent: :nullify
-  validates :uid, presence: true
 
   def update_videos
     channel = Yt::Channel.new id: uid
     channel.videos.each do |yt_video|
       video = videos.find_or_initialize_by(uid: yt_video.id)
-      video.title = yt_video.title
-      video.link = "https://www.youtube.com/watch?v=#{yt_video.id}"
-      video.description = yt_video.description
-      video.channel_uid = uid
-      video.channel_id = id
+      video.fill_details(yt_video)
       video.save(validate: false)
     end
   end

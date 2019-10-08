e13d90f0cc25a6940c4c8ef9bba341a3fa6b9380
TheWillGabriel <will.gabriel@gmail.com>
2019-10-08 09:14:52 -0600

    Create add_videos method for Channel

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,18 @@
 class Channel < ApplicationRecord
   has_many :videos
+  validates :uid, presence: true
+
+  private
+
+    def add_videos
+      channel = YT::Channel.new id: uid
+      channel.videos.each do |yt_video|
+        video = videos.find_or_initialize_by(uid: yt_video.id)
+        video.title = yt_video.title
+        video.link = "https://www.youtube.com/watch?v=#{yt_video.id}"
+        video.description = yt_video.description
+        video.channel_uid = uid
+        video.save
+      end
+    end
 end

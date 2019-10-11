a699286b7e0bc621d081bad59b3d33fef3306b69
TheWillGabriel <will.gabriel@gmail.com>
2019-10-11 14:49:53 -0600

    Add query parameters to videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -30,6 +30,6 @@ class VideosController < ApplicationController
   private
 
     def video_params
-      params.require(:video).permit(:uid)
+      params.require(:video).permit(:uid, :q)
     end
 end

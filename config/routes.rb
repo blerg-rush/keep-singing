5379970215ec35aceb668b576b88c4a7f33234d0
TheWillGabriel <will.gabriel@gmail.com>
2019-10-12 10:05:05 -0600

    Add simplecov to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
   resources :channels
   root 'videos#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

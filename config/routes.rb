611003a219aec6b3cf93c49a7d618b215cc81c7e
TheWillGabriel <will.gabriel@gmail.com>
2019-10-12 10:46:24 -0600

    Clean up videos routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   get '/search', to: 'videos#search', as: 'search_page'
-  get 'home/index'
-  get 'videos/new'
-  get 'videos/index'
-  get 'videos/show'
-  post 'videos/create'
-  resources :videos
+  resources :videos, only: [:index, :show, :new, :create]
   resources :channels, only: [:index, :create, :destroy]
   root 'videos#index'
   devise_for :users

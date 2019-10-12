e84cdc266b301f52c3e1fcb41c455d535f9eeea0
TheWillGabriel <will.gabriel@gmail.com>
2019-10-12 10:19:34 -0600

    Clean up channels routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'channels/index'
-  get 'channels/create'
-  get 'channels/destroy'
   get '/search', to: 'videos#search', as: 'search_page'
   get 'home/index'
   get 'videos/new'
@@ -9,7 +6,7 @@ Rails.application.routes.draw do
   get 'videos/show'
   post 'videos/create'
   resources :videos
-  resources :channels
+  resources :channels, only: [:index, :create, :destroy]
   root 'videos#index'
   devise_for :users
 end

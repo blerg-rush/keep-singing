80d99e20876f0b8db48fae2bfb8c77665a0affba
TheWillGabriel <will.gabriel@gmail.com>
2019-10-13 13:52:05 -0600

    Add to non-embeddable channels list

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  NON_EMBEDDABLE_CHANNELS = %w[UCbqcG1rdt9LMwOJN4PyGTKg].freeze
+  NON_EMBEDDABLE_CHANNELS = %w[UCbqcG1rdt9LMwOJN4PyGTKg
+                               UCYi9TC1HC_U2kaRAK6I4FSQ].freeze
 
   def trim(title)
     # existing

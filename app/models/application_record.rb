0c17ff598cd32a6a8ed7898abdc73f16a0be3b6e
TheWillGabriel <will.gabriel@gmail.com>
2019-10-13 13:25:42 -0600

    Improve 'karaoke' trim regex

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,7 +6,7 @@ class ApplicationRecord < ActiveRecord::Base
   def trim(title)
     # existing
     title.to_s.gsub(/[\(\[]?karaoke version[\)\}]?/i, '')
-         .gsub(/karaoke/i, '')
+         .gsub(/[\(\[]?karaoke[\)\}]?/i, '')
          .squish
   end
 end

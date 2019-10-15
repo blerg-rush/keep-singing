99b2e5d69dc8b68043a5aaafc583e61d28910dde
TheWillGabriel <will.gabriel@gmail.com>
2019-10-15 11:40:22 -0600

    Configure vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest

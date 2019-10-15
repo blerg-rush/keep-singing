a95d46c10523af05f5491b28a1eab8d60e53c96f
TheWillGabriel <will.gabriel@gmail.com>
2019-10-15 16:56:56 -0600

    Add uid tests to video spec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  before(:all) do
+  before(:each) do
     @video1 = create(:video)
   end
 
   it 'is valid with valid attributes' do
     expect(@video1).to be_valid
+  end
+
+  it 'is not valid without a uid' do
+    @video1.uid = nil
+    expect(@video1).to_not be_valid
+  end
+
+  it 'has a unique uid' do
+    video2 = build(:video)
+    expect(video2).to_not be_valid
   end
 end

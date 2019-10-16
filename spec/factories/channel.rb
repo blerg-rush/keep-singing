230de9fac4b0bdfdd5add48908f9ba55ad4fe7e5
TheWillGabriel <will.gabriel@gmail.com>
2019-10-16 10:04:34 -0600

    Add channel spec

---
+++ spec/factories/channel.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :channel do
+    uid { 'YjP0iGnhVo2FXzG9rpQVktWf' }
+    title { 'Test Channel' }
+  end
+end
---
+++ spec/models/channel_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Channel, type: :model do
+  before(:each) do
+    @channel1 = create(:channel)
+  end
+end
